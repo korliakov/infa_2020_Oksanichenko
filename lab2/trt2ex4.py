f0b1d02a2db45c59b8fbd88428ee165e4715deff
OksanichenkoFedor <okssolotheodor@gmail.com>
2020-09-24 10:40:01 +0300

    Первая лаба по пай гейм

--- lab2/trt2ex4.py
+++ lab2/trt2ex4.py
@@ -3,8 +3,8 @@ import turtle as trt
 import numpy as np
 
 trt.shape('turtle')
-trt.speed(50)
-steps_of_time_number = 1000
+trt.speed(1000)
+steps_of_time_number = 10000
 x = randint(-200, 200)
 y = randint(-200, 200)
 vx = randint(-30, 30)
@@ -12,11 +12,18 @@ vy = randint(-30, 30)
 ay = -1
 dt = 0.1
 trt.penup()
-trt.goto(x,y)
-trt.pendown() 
+trt.goto(x, y)
+trt.pendown()
 for i in range(steps_of_time_number):
-    x += vx*dt
-    y += vy*dt + ay*dt**2/2
-    vy += ay*dt
-    trt.goto(x,y)    
-
+    x += vx * dt
+    y += vy * dt + ay * dt ** 2 / 2
+    vy += ay * dt
+    trt.goto(x, y)
+    if x > 250:
+        vx = (-1) * vx
+    if x < -250:
+        vx = (-1) * vx
+    if y > 250:
+        vy = (-1) * vy
+    if y < -250:
+        vy = (-1) * vy
